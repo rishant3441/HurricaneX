aff2ecc6caa3aa54fb305a1fe55de231b762ba29
Rishan Thangaraj <rishanthangaraj@gmail.com>
2024-08-08 18:30:50 -0400

    add getData

--- src/firebase/addData.js
+++ src/firebase/addData.js
@@ -1,7 +1,6 @@
-import { firebase_app } from "./config";
+import { db, firebase_app } from "./config";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-const db = getFirestore(firebase_app, "(default)")
 export default async function addData(colllection, id, data) {
     let result = null;
     let error = null;
---
+++ src/firebase/getData.js
@@ -0,0 +1,17 @@
+import { db, firebase_app } from "./config";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+export default async function getDoument(collection, id) {
+    let docRef = doc(db, collection, id);
+
+    let result = null;
+    let error = null;
+
+    try {
+        result = await getDoc(docRef);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
