104fecbacb2971017c853fe5337df518daf4229b
Rishan Thangaraj <rishanthangaraj@gmail.com>
2024-08-10 20:15:44 -0400

    add mapbox

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,6 +2,9 @@
 import React from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import Map from "react-map-gl";
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function Page() {
     const { user } = useAuthContext()
@@ -11,7 +14,21 @@ function Page() {
         if (user == null) router.push("/sign-in")
     }, [user])
 
-    return (<h1>our super cool dashboard</h1>);
+    return (
+        <div>
+            <Map
+                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+                initialViewState={{
+                    latitude: 26.609,
+                    longitude: -80.352,
+                    zoom: 9.24
+                }}
+                style={{width: 600, height: 400}}
+                mapStyle="mapbox://styles/mapbox/streets-v12"
+            >
+            </Map>
+        </div>
+    );
 }
 
 export default Page;
