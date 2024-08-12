0759ca52a7f57f21be0d5505f163d14fb282ec37
Rishan Thangaraj <rishanthangaraj@gmail.com>
2024-08-11 22:28:51 -0400

    Redirect to dashboard on log in

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -28,7 +28,7 @@ function Page() {
     // Redirect to the admin page
     // Typically you would want to redirect them to a protected page an add a check to see if they are admin or 
     // create a new page for admin
-    router.push( "/" );
+    router.push( "/dashboard" );
   }
 
   return (
