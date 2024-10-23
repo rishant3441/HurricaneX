0260c09618cdc45b33a2e43aca112d1d34b8183d
Rishan Thangaraj <rishanthangaraj@gmail.com>
2024-10-23 19:11:29 -0400

    Fix cors issue temporarily

--- src/context/apollo-client.js
+++ src/context/apollo-client.js
@@ -2,8 +2,15 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "https://www.gasbuddy.com/graphql",
+    uri: "https://proxy.cors.sh/https://www.gasbuddy.com/graphql",
     cache: new InMemoryCache(),
+    headers: {
+      "content-type": "application/json",
+      'x-cors-api-key': 'temp_aa1a6a2ae98ec6afbef4341e0d0b25e0'
+    },
+    fetchOptions: {
+      mode: 'no-cors'
+    }
   });
 };
 
