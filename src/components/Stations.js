7fc36d2182301b1c327e2ab261fe1380ae77a3e8
sdragonslayer <sydu2021@gmail.com>
2024-10-23 20:12:14 -0400

    error for near me

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -36,6 +36,33 @@ query LocationBySearchTerm($brandId: Int, $cursor: String, $fuel: Int, $lat: Flo
 `;
 
 export default function Stations({ setStations, userCoordinates }) {
+
+  if (!userCoordinates || userCoordinates.lat == null || userCoordinates.lng == null) {
+      const { data, loading, error } = useQuery(QUERY, {
+        variables: {
+          lat: 26.7056,
+          lng: -80.0364,
+          search: "", // Default search term
+        },
+      });
+
+      useEffect(() => {
+        console.log(data)
+        console.log(loading)
+        console.log(error)
+        if (data) {
+          const stations = data.locationBySearchTerm.stations.results.map(station => ({
+            name: station.name,
+            address: `${station.address.line1} ${station.address.locality} ${station.address.region} ${station.address.postalCode} ${station.address.country}`
+          }));
+          setStations(stations);
+        }
+      }, [data, setStations]);
+
+      return <h1 style={{ color: 'white', backgroundColor: 'red', padding: '10px', textAlign: 'center', fontSize: '20px', borderRadius: '8px' }}>
+        Error: Missing current location, local gas stations can not be shown. Please enable location access </h1>;
+    }
+  
   const { data, loading, error } = useQuery(QUERY, {
     variables: {
       lat: userCoordinates.lat,
